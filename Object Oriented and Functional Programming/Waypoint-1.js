ac1c28be45b72ca32906460e23c5ab2dda49a8cf
Amanda P <ohkamanda@gmail.com>
2015-12-03 17:08:03 -0500

    Object Oriented and Functional Programming
    Waypoints 1 - 4

---
+++ Object Oriented and Functional Programming/Waypoint-1.js
@@ -0,0 +1,14 @@
+/*
+Waypoint: Declare JavaScript Objects as Variables
+
+Before we dive into Object Oriented Programming, let's revisit JavaScript objects.
+
+Give your motorBike object a wheels, engines and seats attribute and set them to numbers.
+*/
+
+var motorBike = {
+	"wheels": 2,
+	"engines": 1,
+	"seats": 2
+};
+
