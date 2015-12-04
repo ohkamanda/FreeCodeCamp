6900c608e5f4f86d890ea44642394a07ec806277
Amanda P <ohkamanda@gmail.com>
2015-12-03 20:02:43 -0500

    Basic Algorithm Scripting

---
+++ Basic Algorithm Scripting/01 -  Meet Bonfire.js
@@ -0,0 +1,21 @@
+/*
+Bonfire: Meet Bonfire
+
+Author: http://www.freecodecamp.com/ohkamanda
+Challenge: http://www.freecodecamp.com/challenges/bonfire-meet-bonfire
+
+Your goal is to fix the failing test.
+
+First, run all the tests by clicking "Run tests" or by pressing Control + Enter.
+
+The failing test is in red. Fix the code so that all tests pass. Then you can move on to the next Bonfire.
+
+Make this function return true no matter what.
+*/
+
+function meetBonfire(argument) {
+  console.log("you can read this function's argument in the developer tools", argument);
+  return true;
+}
+
+meetBonfire("You can do this!");
---
+++ Basic Algorithm Scripting/02 - Reverse a String.js
@@ -0,0 +1,20 @@
+/*
+Bonfire: Reverse a String
+
+Author: http://www.freecodecamp.com/ohkamanda
+Challenge: http://www.freecodecamp.com/challenges/bonfire-reverse-a-string
+
+Reverse the provided string.
+
+You may need to turn the string into an array before you can reverse it.
+
+Your result must be a string.
+*/
+
+function reverseString(str) {
+	// split() returns an array on which reverse() and join() can be applied
+	var newStr = str.split("").reverse().join("");
+	return newStr;
+}
+
+reverseString("hello");
