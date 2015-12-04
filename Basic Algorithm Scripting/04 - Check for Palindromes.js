f4325534ea64670a776be94e2e12b15cf20eec8f
Amanda P <ohkamanda@gmail.com>
2015-12-04 01:19:27 -0500

    changes

--- Basic Algorithm Scripting/04 - Check for Palindromes.js
+++ Basic Algorithm Scripting/04 - Check for Palindromes.js
@@ -1,6 +1,9 @@
 /*
 Bonfire: Check for Palindromes
 
+Author: http://www.freecodecamp.com/ohkamanda
+Challenge: http://www.freecodecamp.com/challenges/bonfire-check-for-palindromes
+
 Return true if the given string is a palindrome. Otherwise, return false.
 
 A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
@@ -9,11 +12,8 @@ You'll need to remove all non-alphanumeric characters (punctuation, spaces and s
 
 We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
 
-Author: http://www.freecodecamp.com/ohkamanda
-Challenge: http://www.freecodecamp.com/challenges/bonfire-check-for-palindromes
 Helpful Resources: (1) http://ironion.com/bonfire-reverse-a-string-solution/
 (2) https://github.com/Rafase282/My-FreeCodeCamp-Code
-
 
 */
 
