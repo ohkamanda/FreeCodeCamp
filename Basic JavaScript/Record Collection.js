b7782696376fe4da277e02604c7fac95fbe3bbd4
Amanda P <ohkamanda@gmail.com>
2016-02-03 23:01:55 -0500

    commit

---
+++ Basic JavaScript/Record Collection.js
@@ -0,0 +1,30 @@
+/*
+Record Collection
+
+Author: http://ohkamanda.github.io/
+Challenge: http://www.freecodecamp.com/challenges/accessing-nested-objects-in-json
+
+Retrieve the second tree from the variable myPlants 
+using object dot and array bracket notation.
+*/
+
+var myPlants = [
+  { 
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }  
+];
+
+var secondTree = myPlants[1].list[1];
