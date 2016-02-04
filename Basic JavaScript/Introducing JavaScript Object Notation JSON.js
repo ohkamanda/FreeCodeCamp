9e5428b1c6f040153d79b05efbe03405dfb0390e
Amanda P <ohkamanda@gmail.com>
2016-02-03 22:31:46 -0500

    commit

---
+++ Basic JavaScript/Introducing JavaScript Object Notation JSON.js
@@ -0,0 +1,31 @@
+/*
+Introducing JavaScript Object Notation JSON
+
+Author: http://ohkamanda.github.io/
+Challenge: http://www.freecodecamp.com/challenges/introducing-javascript-object-notation-json
+
+Add a new album to the myMusic JSON object. Add artist and title strings, 
+release_year number, and a formats array of strings.
+*/
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CS", 
+      "8T", 
+      "LP" ],
+    "gold": true
+  },
+  // Add record here
+ {
+    "artist": "Brand New",
+    "title": "The Devil and God are Raging Inside",
+    "release_year": 2006,
+    "formats": [ 
+      "CD", 
+      "Vinyl" ]
+ }
+];
