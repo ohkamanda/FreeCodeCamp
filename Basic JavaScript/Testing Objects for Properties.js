d6f7b91edef16a47cb15c165a0f59081b66f9a81
Amanda P <ohkamanda@gmail.com>
2016-02-03 22:04:32 -0500

    commit

---
+++ Basic JavaScript/Testing Objects for Properties.js
@@ -0,0 +1,24 @@
+/*
+Using Objects for Lookups
+
+Author: http://ohkamanda.github.io/
+Challenge: http://www.freecodecamp.com/challenges/testing-objects-for-properties
+
+Modify the function checkObj to test myObj for checkProp. If the property is found, 
+return that property's value. If not, return "Not Found".
+*/
+
+var myObj = {
+  gift: "pony",
+  pet: "kitten",
+  bed: "sleigh"
+};
+
+function checkObj(checkProp) {
+  if (myObj.hasOwnProperty(checkProp)) {
+    return myObj.checkProp;
+  } else { return "Not Found";}
+}
+
+// Test your code by modifying these values
+checkObj("gift");
